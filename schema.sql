33d18dffdd82ed834dccef48dbb446cd8d2dc4fa
karorot <174889933+karorot@users.noreply.github.com>
2025-09-14 15:03:42 +0300

    Add label elements and more inputs to registration
    Add input requirements to user registration that were previously missing:
    user location and name. In addition, add label elements for better
    usability.

--- schema.sql
+++ schema.sql
@@ -2,4 +2,8 @@ CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     username TEXT UNIQUE,
     password_hash TEXT
-)
+)    password_hash TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    location TEXT
+);
