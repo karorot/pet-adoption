e18dbf606ffef3e4e2c5fa651a66c08f8ce2f10d
karorot <174889933+karorot@users.noreply.github.com>
2025-10-04 19:29:10 +0300

    Remove redundant items from schema
    Remove redundant items from the table 'pets' which are now
    handled by the separate table 'classes'.

--- schema.sql
+++ schema.sql
@@ -9,10 +9,7 @@ CREATE TABLE pets (
     id INTEGER PRIMARY KEY,
     name TEXT,
     birth_year INTEGER,
-    pet_type TEXT,
     breed TEXT,
-    gender TEXT,
-    size TEXT,
     description TEXT,
     user_id INTEGER REFERENCES users,
     posted_at TEXT
